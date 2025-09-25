a3d0a386701e529022af7881dc56663af92d10eb
Vishwajeet-keni <kenivishwajeet874@gmail.com>
2025-09-25 15:20:59 +0530

    debugging and sorg of optamizing

--- num_BaseCov(2,16,8,10).py
+++ num_BaseCov(2,16,8,10).py
@@ -38,117 +38,122 @@ def valid_input():
 def valid_required_output(x):
  global re_run
  if x.endswith(".BIN"):
-     x=(x[:-4]) #removes last 3 terms 
      y=input("\nEnter_type_of_output_you_require_|" 
              "\n.hex: Hexadecimal .eg(Enter-> .hex)"
              "\n.oct: Octal       .eg(Enter-> .oct)"
              "\n.dec: Decimal     .eg(Enter-> .dec)"
              "\nOutput: ").upper()
-     # catching Error in required ourput request
-     try:
-         print("\n___________________________________"
-               "\nRequired_Output_Error_Handler:"
-               "\n-----------------------------------")
-         if y.endswith(".BIN") or y.endswith(".HEX") or y.endswith(".OCT") or y.endswith(".DEC"):
-             print("Valid Input")
-             re_run=""
-         else:
-             re_run=input("Invalid Input"
-                          "\nCode Terminated"
-                          "\n-----------------------------------"
-                          "\nWant to run the program again(Y/N):").upper()
-     finally:
-         if re_run=="Y":
-             print("Re running program..."
-                  "\n___________________________________")
-             y=valid_required_output()
-             return y 
-         elif re_run=="N":
+ elif x.endswith(".HEX"):
+    y=input("\nEnter_type_of_output_you_require__|" 
+            "\n.bin: Binary      .eg(Enter-> .bin)"
+            "\n.oct: Octal       .eg(Enter-> .oct)"
+            "\n.dec: Decimal     .eg(Enter-> .dec)"
+            "\nOutput: ").upper()
+ elif x.endswith(".OCT"):
+    y=input("\nEnter_type_of_output_you_require__|" 
+            "\n.bin: Binary      .eg(Enter-> .bin)"
+            "\n.hex: Hexadecimal .eg(Enter-> .hex)"
+            "\n.dec: Decimal     .eg(Enter-> .dec)"
+            "\nOutput: ").upper()
+ elif x.endswith(".DEC"):
+    y=input("\nEnter_type_of_output_you_require__|" 
+            "\n.bin: Binary      .eg(Enter-> .bin)"
+            "\n.hex: Hexadecimal .eg(Enter-> .hex)"
+            "\n.oct: Octal       .eg(Enter-> .oct)"
+            "\nOutput: ").upper()
+# catching Error in required ourput request
+ try:
+     print("\n___________________________________"
+          "\nRequired_Output_Error_Handler:"
+          "\n-----------------------------------")
+     if y.endswith(".BIN") or y.endswith(".HEX") or y.endswith(".OCT") or y.endswith(".DEC"):
+        print("Valid Input")
+        re_run=""
+     else:
+        re_run=input("Invalid Input"
+                     "\nCode Terminated"
+                     "\n-----------------------------------"
+                     "\nWant to run the program again(Y/N):").upper()
+ finally:
+     if re_run=="Y":
+        print("Re running program..."
+              "\n___________________________________")
+        y=valid_required_output()
+        return y 
+     elif re_run=="N":
           print("Exiting program..."
                 "\n___________________________________")
-         else:
-            print("___________________________________")
-            return y
+     else:
+        print("___________________________________")
+        return y
 
 x=valid_input()
 y=valid_required_output(x) 
 
-if x.endswith("BIN"):
-    x=(x[:-4]) #removes last 3 terms 
-    y=input("\nEnter_type_of_output_you_require_|" 
-            "\n.hex: Hexadecimal .eg(Enter-> .hex)"
-            "\n.oct: Octal       .eg(Enter-> .oct)"
-            "\n.dec: Decimal     .eg(Enter-> .dec)"
-            "\nOutput: ").upper()
-    if y==".HEX":
-        y=Conv.B2H(x)
-        print(f"\nYour Input: {x}.bin      \
-                \nRequired Output: {y}.hex")
-    elif y==".OCT":
-        y=Conv.B2O(x)
-        print(f"\nYour Input: {x}.bin      \
-                \nRequired Output: {y}.oct")
-    elif y==".DEC":
-        y=Conv.B2D(x)
-        print(f"\nYour Input: {x}.bin      \
-                \nRequired Output: {y}.dec")
+#bin-->hex,oct,dec
+if x.endswith(".BIN") and y==".HEX":
+    x=(x[:-4]) #removes last 3 terms
+    y=Conv.B2H(x)
+    print(f"\nYour Input: {x}.bin      \
+            \nRequired Output: {y}.hex")
+elif x.endswith(".BIN") and y==".OCT":
+    x=(x[:-4])
+    y=Conv.B2O(x)
+    print(f"\nYour Input: {x}.bin      \
+            \nRequired Output: {y}.oct")
+elif x.endswith(".BIN") and y==".DEC":
+    x=(x[:-4])
+    y=Conv.B2D(x)
+    print(f"\nYour Input: {x}.bin      \
+            \nRequired Output: {y}.dec")
         
-elif x.endswith("HEX"):
+#hex-->bin,oct,dec
+elif x.endswith(".HEX") and y==".BIN":
     x=x[:-4]
-    y=input("\nEnter_type_of_output_you_require__|" 
-            "\n.bin: Binary      .eg(Enter-> .bin)"
-            "\n.oct: Octal       .eg(Enter-> .oct)"
-            "\n.dec: Decimal     .eg(Enter-> .dec)"
-            "\nOutput: ").upper()
-    if y==".BIN":
-        y=Conv.H2B(x)
-        print(f"\nYour Input: {x}.hex      \
-                \nRequired Output: {y}.bin")
-    elif y==".OCT":
-        y=Conv.H2O(x)
-        print(f"\nYour Input: {x}.hex      \
-                \nRequired Output: {y}.oct")
-    elif y==".DEC":
-        y=Conv.H2D(x)
-        print(f"\nYour Input: {x}.hex      \
-                \nRequired Output: {y}.oct")
+    y=Conv.H2B(x)
+    print(f"\nYour Input: {x}.hex      \
+            \nRequired Output: {y}.bin")
+elif x.endswith(".HEX") and  y==".OCT":
+    x=x[:-4]
+    y=Conv.H2O(x)
+    print(f"\nYour Input: {x}.hex      \
+            \nRequired Output: {y}.oct")
+elif x.endswith(".HEX") and  y==".DEC":
+    x=x[:-4]
+    y=Conv.H2D(x)
+    print(f"\nYour Input: {x}.hex      \
+            \nRequired Output: {y}.oct")
         
-elif x.endswith("OCT"):
+#oct-->bin,hex,dec
+elif x.endswith(".OCT") and y==".BIN":
     x=x[:-4]
-    y=input("\nEnter_type_of_output_you_require__|" 
-            "\n.bin: Binary      .eg(Enter-> .bin)"
-            "\n.hex: Hexadecimal .eg(Enter-> .hex)"
-            "\n.dec: Decimal     .eg(Enter-> .dec)"
-            "\nOutput: ").upper()
-    if y==".BIN":
-        y=Conv.O2B(x)
-        print(f"\nYour Input: {x}.oct      \
-                \nRequired Output: {y}.bin")
-    elif y==".HEX":
-        y=Conv.O2H(x)
-        print(f"\nYour Input: {x}.oct      \
-                \nRequired Output: {y}.hex")
-    elif y==".DEC":
-        y=Conv.O2D(x)
-        print(f"\nYour Input: {x}.oct      \
-                \nRequired Output: {y}.dec")
+    y=Conv.O2B(x)
+    print(f"\nYour Input: {x}.oct      \
+            \nRequired Output: {y}.bin")
+elif x.endswith(".OCT") and  y==".HEX":
+    x=x[:-4]
+    y=Conv.O2H(x)
+    print(f"\nYour Input: {x}.oct      \
+            \nRequired Output: {y}.hex")
+elif x.endswith(".OCT") and  y==".DEC":
+    x=x[:-4]
+    y=Conv.O2D(x)
+    print(f"\nYour Input: {x}.oct      \
+            \nRequired Output: {y}.dec")
         
-elif x.endswith("DEC"):
+#dec-->bin,hex,oct
+elif x.endswith("DEC") and y==".BIN":
     x=x[:-4]
-    y=input("\nEnter_type_of_output_you_require__|" 
-            "\n.bin: Binary      .eg(Enter-> .bin)"
-            "\n.hex: Hexadecimal .eg(Enter-> .hex)"
-            "\n.dec: Decimal     .eg(Enter-> .oct)"
-            "\nOutput: ").upper()
-    if y==".BIN":
-        y=Conv.D2B(x)
-        print(f"\nYour Input: {x}.dec      \
-                \nRequired Output: {y}.bin")
-    elif y==".HEX":
-        y=Conv.D2H(x)
-        print(f"\nYour Input: {x}.dec      \
-                \nRequired Output: {y}.hex")
-    elif y==".OCT":
-        y=Conv.D2O(x)
-        print(f"\nYour Input: {x}.dec      \
-                \nRequired Output: {y}.oct")
+    y=Conv.D2B(x)
+    print(f"\nYour Input: {x}.dec      \
+            \nRequired Output: {y}.bin")
+elif x.endswith("DEC") and  y==".HEX":
+    x=x[:-4]
+    y=Conv.D2H(x)
+    print(f"\nYour Input: {x}.dec      \
+            \nRequired Output: {y}.hex")
+elif x.endswith("DEC") and  y==".OCT":
+    x=x[:-4]
+    y=Conv.D2O(x)
+    print(f"\nYour Input: {x}.dec      \
+            \nRequired Output: {y}.oct")
